715f96156952d4f18c705965e78b2359a2e92cc3
nozimjonpro <nozimjonkeldiyev@yandex.ru>
2021-12-16 11:17:23 +0500

    Add some features

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const elHambergur = document.querySelector('.hambergur');
-const elHeader = document.querySelector('.header')
+const cHambergur = document.querySelector('.hambergur__close')
+const elHeader = document.querySelector('.header');
 
 elHambergur.addEventListener('click', ()=>{
 elHeader.classList.toggle('header--active')
 })
+
+cHambergur.addEventListener('click', ()=>{
+    elHeader.classList.toggle('header--active')
+    })
